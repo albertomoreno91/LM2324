2a5fa352753513efd757dc87154c27ea88839753
albertomoreno91 <albertomoreno91@iesamachado.org>
2024-01-11 14:26:24 +0100

    java

--- tema3/java6.js
+++ tema3/java6.js
@@ -1,1 +1,9 @@
-function ocultar(caja1)document.getElementById('caja1').style.visibility
+function ocultarRojo() {
+    document.getElementById('caja1').style.visibility = "hidden"
+}
+function ocultarVerde() {
+    document.getElementById('caja2').style.visibility = "hidden"
+}
+function ocultarAzul() {
+    document.getElementById('caja3').style.visibility = "hidden"
+}
