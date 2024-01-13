d98762c1aa203d12938e9a61af148918859ea07d
Alberto Moreno <albertomoreno91@iesamachado.org>
2024-01-14 00:58:57 +0100

    java

--- tema3/java6.js
+++ tema3/java6.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function ocultar(x) {
     document.getElementById(x).style.visibility = "hidden"
 }
@@ -6,20 +5,3 @@ function ocultar(x) {
 function mostrar(x) {
     document.getElementById(x).style.visibility = "visible"
 }
-=======
-function ocultarRojo() {
-    if (document.getElementById('caja1').style.visibility == "visible")
-        document.getElementById('caja1').style.visibility = "hidden"
-    else document.getElementById('caja1').style.visibility = "visible"
-}
-function ocultarVerde() {
-    if (document.getElementById('caja2').style.visibility == "visible")
-        document.getElementById('caja2').style.visibility = "hidden"
-    else document.getElementById('caja2').style.visibility = "visible"
-}
-function ocultarAzul() {
-    if (document.getElementById('caja3').style.visibility == "visible")
-        document.getElementById('caja3').style.visibility = "hidden"
-    else document.getElementById('caja3').style.visibility = "visible"
-}
->>>>>>> 8330f5b207dd1c9077f6fac64c39283b65881d8d
