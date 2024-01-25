4ab1e027417e5a474d08b91d1d975ba48026b7bb
Alberto Moreno <albertomoreno91@iesamachado.org>
2024-01-25 19:44:07 +0100

    array

--- tema3/numreales.js
+++ tema3/numreales.js
@@ -4,7 +4,7 @@ function anadir() {
     if (document.getElementById('numero').value == "")
         alert('Intruduzca un número')
     else {
-        numeros.push(document.getElementById('numero').value)
+        numeros.push(parseFloat(document.getElementById('numero').value))
         document.getElementById('array').innerHTML = numeros.join(', ')
         document.getElementById('numero').value = ""
     }
@@ -16,11 +16,14 @@ function sumar() {
     }
 
     else {
-        let suma = numeros.reduce(funcionsuma)
-        function funcionsuma(total, value) {
-            return total + value
+        function sumarArray(array) {
+            let suma = 0;
+            for (let i = 0; i < array.length; i++) {
+                suma += array[i];
+            }
+            return suma;
         }
-        document.getElementById('resultado').innerHTML = suma
+        document.getElementById('resultado').innerHTML = sumarArray(numeros)
     }
 }
 
@@ -28,9 +31,20 @@ function multiplicar() {
     if (numeros.length < 2) {
         alert('Introduzca al menos dos números en la cadena')
     }
+
+    else {
+        function multArray(array) {
+            for (let i = 0; i < array.length; i++) {
+                mult += array[i];
+            }
+            return mult;
+        }
+        document.getElementById('resultado').innerHTML = multArray(numeros)
+    }
 }
 
 function reset() {
     numeros = []
     document.getElementById('array').innerHTML = numeros
+    document.getElementById('resultado').innerHTML = ""
 }
