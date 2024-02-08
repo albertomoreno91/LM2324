96048295d328de19de08168d52fd0c4a9210c193
Alberto Moreno <albertomoreno91@iesamachado.org>
2024-02-08 20:49:17 +0100

    mayorelemento

--- tema3/mayorelemento.js
+++ tema3/mayorelemento.js
@@ -1,1 +1,27 @@
 let cadena = []
+
+function anadir() {
+    let numero = parseFloat(document.getElementById('numero').value)
+    if (isNaN(numero))
+        alert('Introduzca un número')
+    else {
+        cadena.push(numero)
+        document.getElementById('cadena').innerHTML = cadena.join(', ')
+        document.getElementById('numero').value = ""
+    }
+}
+
+function calcular() {
+    if (cadena.length < 2)
+        alert('Introduzca al menos dos números en la cadena')
+    else {
+        cadena.sort(function (a, b) { return b - a })
+        document.getElementById('resultado').innerHTML = cadena[0]
+    }
+}
+
+function reset() {
+    cadena = []
+    document.getElementById('resultado').innerHTML = ""
+    document.getElementById('cadena').innerHTML = ""
+}
